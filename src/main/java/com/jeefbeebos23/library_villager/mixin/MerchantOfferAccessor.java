f560bf08768483e404d9d1591dc6300103c2f72f
Aidan Guinn <wbguinn77@gmail.com>
2026-05-15 18:35:38 -0700

    feat: add MerchantOfferAccessor placeholder

---
+++ src/main/java/com/jeefbeebos23/library_villager/mixin/MerchantOfferAccessor.java
@@ -0,0 +1,8 @@
+package com.jeefbeebos23.library_villager.mixin;
+
+import net.minecraft.world.item.trading.MerchantOffer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MerchantOffer.class)
+public interface MerchantOfferAccessor {
+}
