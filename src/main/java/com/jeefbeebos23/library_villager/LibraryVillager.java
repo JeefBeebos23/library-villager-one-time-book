a2e8a94f1508f45e8fb9949a04b27986d0ad1e39
Aidan Guinn <wbguinn77@gmail.com>
2026-05-15 18:12:33 -0700

    chore: scaffold project

---
+++ src/main/java/com/jeefbeebos23/library_villager/LibraryVillager.java
@@ -0,0 +1,11 @@
+package com.jeefbeebos23.library_villager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LibraryVillager implements ModInitializer {
+    public static final String MOD_ID = "library_villager";
+
+    @Override
+    public void onInitialize() {
+    }
+}
